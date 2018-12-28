2b89de7b0cf5c95a04ebd11217956704e2c4ec17
unknown <Robokatbot@gmail.com>
2018-12-28 19:15:02 +0800

    simplified exec

--- commands/bash.js
+++ commands/bash.js
@@ -1,7 +1,7 @@
 const {exec} = require('child_process');
 
 exports.run = async (client,message,args)=>{
-	exec(`echo ${args.join(' ').replace(/\|/,'\\|')} | bash`,(error,stdout,stderr)=>{
+	exec(`${args.join(' ').replace(/\|/,'\\|')}`,(error,stdout,stderr)=>{
 		message.channel.send(`Output: ${error||stdout||stderr}`);
 	})
 }
