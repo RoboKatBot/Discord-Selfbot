28c6654f9cca187aea82360db48a3663e8099290
unknown <Robokatbot@gmail.com>
2019-09-13 20:34:49 +0800

    wolfram.js

---
+++ commands/wolfram.js
@@ -0,0 +1,27 @@
+const {spawn} = require('child_process');
+var proc;
+
+exports.run = async (client,message,args)=>{
+	if(!proc) {
+		proc = spawn("Wolfram");
+		proc.stdout.on('data',(data)=>{
+			message.channel.send(`\`\`\`Mathematica\n${data}\`\`\``);
+		});
+	}
+	proc.write(args.join(' ').trim()+'\n');
+}
+
+exports.init = (client)=>{
+	//optional
+}
+
+exports.conf = {
+	aliases:[mathematica, math, maths]
+}
+
+exports.help = {
+	name:"wolfram",
+	desc:"open/use a wolfram repl session",
+	usage:"wolfram [code]",
+	extended:""
+}
