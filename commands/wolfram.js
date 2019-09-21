d3870c1d50cd6ddaa4ccbab50a920ce746790600
unknown <Robokatbot@gmail.com>
2019-09-21 21:26:25 +0800

    graphics handling

--- commands/wolfram.js
+++ commands/wolfram.js
@@ -3,11 +3,15 @@ var proc;
 
 exports.run = async (client,message,args)=>{
 	if(!proc) {
-		proc = spawn("wolfram");
+		proc = spawn("wolfram -initfile ~/.wolfram");
 		proc.stdout.on('data',(data)=>{
 			data = String(data).trim().replace(/In\[\d+\]\:=/,"").replace(`Wolfram Language 11.3.0 Engine for Linux ARM (32-bit)
 Copyright 1988-2018 Wolfram Research, Inc.`,"");
 			if (data) {
+				if (a = data.match(/(Out\[\d+\]=) -Graphics-/)) {
+					message.channel.send(`\`\`\`Mathematica\n${a[0]}\`\`\``,{files:[{name:"/tmp/out.png"}]})
+					return
+				}
 				data = data.match(/[\s\S]{1,1950}(?=\n|$)/g);
 				data.forEach(k=>message.channel.send(`\`\`\`Mathematica\n${k}\`\`\``));
 			}
