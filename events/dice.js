9bb15e45d66d4e802149915d463519a04d96361b
unknown <Robokatbot@gmail.com>
2020-01-13 22:44:37 +0800

    meme

--- events/dice.js
+++ events/dice.js
@@ -3,6 +3,7 @@ const config = require('../config.json');
 
 module.exports = async (client,message)=>{
 	const parsed = message.content.match(/^\\r\s*(\d*)d(\d+)/);
+	console.log(parsed);
 	const req = [parseInt(parsed[1]),parseInt(parsed[2])];
 	const res = roll(...req);
 	send(`
@@ -30,7 +31,7 @@ function send(content) {
 
 	const webhook = client.guilds.get('666236320745652224').fetchWebhooks()
 		.then(webhooks=>webhooks.filter(webhook=>webhook.channelID=='666257084941074442').first())
-		
+
 	webhook.then(webhook=>webhook.send(content,{
 			avatarURL:'https://cdn.discordapp.com/attachments/349488875561025536/666277457564794900/unknown.png',
 			username:'DiceGuy'
