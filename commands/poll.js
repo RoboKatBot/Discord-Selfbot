5871d2b53757a3529b251d349a2a0e9cb42d8b3f
unknown <Robokatbot@gmail.com>
2020-01-18 20:11:50 +0800

    Fianl? Testing

--- commands/poll.js
+++ commands/poll.js
@@ -4,18 +4,26 @@ exports.run = async (client,message,args)=>{
 	let now = new Date();
 	let then = new Date();
 	let range = parseInt(args[0])||7;
-	for (let i = 1; i < range;i++) {
+	for (let i = 1; i <= range;i++) {
 		then.setDate(now.getDate() + i);
 		let dateString = then.toDateString();
 		client.channels.get('349488875561025536').send(`> ${dateString}`)
 			.then(pollMessage=>{
 				pollMessage.react('👍').catch(e=>console.error(`Error in reacting to poll message: ${e}`));
 				pollMessage.react('👎').catch(e=>console.error(`Error in reacting to poll message: ${e}`));
-				client.on('messageReactionAdd', (Reaction,user)=>{
-					if (user.id===/*'213592879480700928'*/'197319892238598144') {
+				pollMessage.react('🤷').catch(e=>console.error(`Error in reacting to poll message: ${e}`));
+				client.on('messageReactionAdd', (reaction,user)=>{
+					if (user.id!==/*'213592879480700928'*/'197319892238598144') return;
+					if (reaction._emoji.name==='👎')
 						pollMessage.edit(`~~${pollMessage.content}~~`);
-					}
-				})
+					
+				});
+				client.on('messageReactionRemove', (reaction,user)=>{
+					if (user.id!==/*'213592879480700928'*/'197319892238598144') return;
+					if (reaction._emoji.name==='👎')
+						pollMessage.edit(pollMessage.content.replace(/^~~|~~$/g,''));
+					
+				});
 			});
 	}
 }
