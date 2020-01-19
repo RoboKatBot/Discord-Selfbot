2f75eafaed1df6674ae4d76edbab4899e5a2b5ff
unknown <Robokatbot@gmail.com>
2020-01-19 15:54:01 +0800

    fix

--- utils/dice.js
+++ utils/dice.js
@@ -37,7 +37,7 @@ exports.run = async (client,message)=>{
 		else {
 			Δ = atom.value;
 		}
-		total += Δ*(atom.neg)?-1:1 //negate if needed
+		total += Δ*(atom.neg?-1:1) //negate if needed
 	})
 	response.push(total);
 	if (dice.length==1&&dice[0].value.filter(f=>f.keep).length===1&&dice[0].params[1]==20) { //if dice roll was a single kept d20 
