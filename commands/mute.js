726b32c9588762fd4397257c7736b63a7f80a435
unknown <Robokatbot@gmail.com>
2020-02-13 15:18:49 +0800

    accidental typing fix

--- commands/mute.js
+++ commands/mute.js
@@ -9,7 +9,7 @@ exports.init = async (client)=>{
 		if (!users.map(u=>u.user).includes(m.author)) return;
 		m.delete();
 	});
-	client.on('voiceStateUpdate',(_,user)=>{cd bin
+	client.on('voiceStateUpdate',(_,user)=>{
 		if (!users.includes(user)) return;
 		user.setMute(true,reason);
 	});
